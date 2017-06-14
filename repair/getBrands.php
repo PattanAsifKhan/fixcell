2863c35053cb29cc03dc588353b6ca1d60fc8524
sanjit <sanjitschouhan@gmail.com>
2017-06-15 01:27:36 +0530

    repairs page updated

--- repair/getBrands.php
+++ repair/getBrands.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL | E_STRICT);
-ini_set("display_errors", 1);
-ini_set("html_errors", 1);
+//error_reporting(E_ALL | E_STRICT);
+//ini_set("display_errors", 1);
+//ini_set("html_errors", 1);
 
 start:
 if (!file_exists('sessionid')) {
