877f8610071956436754536bee4812f7c9c22fe4
sanjit <sanjitschouhan@gmail.com>
2017-06-28 16:38:39 +0530

    repairs page updated

--- repair/repair.php
+++ repair/repair.php
@@ -1,3 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fixcell</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/bootstrap.min.css"
+          rel="stylesheet"><!-- Optional theme -->
+    <link href="/css/bootstrap-theme.min.css"
+          rel="stylesheet"><!-- Latest compiled and minified JavaScript -->
+    <link href="/jquery-ui-1.12.1/jquery-ui.min.css" rel="stylesheet">
+    <script src="/jquery-3.1.1.js"></script>
+    <script src="/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <link href="/style.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
 
-?>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+</head>
+<body>
+
+<div class="navbar navbar-inverse"
+     style="-webkit-border-radius: 0;-moz-border-radius: 0;border-radius: 0; margin-bottom: 0px;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar"
+                    style="vertical-align: middle; margin-top: 15px">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/" style="height: auto;">
+                <img src="/images/fixcell.png" alt="fixcell" style="display: inherit;"/>
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav nav-pills nav-justified navbar-right" style="margin-top: 10px;">
+                <li><a href="/">Home</a></li>
+                <li class="active"><a href="/repair/">Repair</a></li>
+                <li><a href="/services">Service</a></li>
+                <li><a href="/faq/">FAQ</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!--footer-->
+<div id="footer"></div>
+<script>
+    $(document).ready(function () {
+        $("#footer").load("/footer.html");
+    });
+</script>
+</body>
+</html>
