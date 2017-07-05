f916564064e2f11bc70a0d7171964265cac68abd
sanjit <sanjitschouhan@gmail.com>
2017-07-05 18:58:57 +0530

    feedback

--- index.php
+++ index.php
@@ -155,11 +155,11 @@ echo "</script>";
         </div>
 
         <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <a class="left carousel-control" href="#feeds" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <a class="right carousel-control" href="#feeds" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right"></span>
             <span class="sr-only">Next</span>
         </a>
@@ -193,22 +193,24 @@ echo "</script>";
             .appendTo(first_inner);
 
         $.each(feedbacks, function (i) {
-            $("<li/>")
-                .attr("data-target", "#feeds")
-                .attr("data-slide-to", "" + i)
-                .appendTo($carousel_indicator);
+            if (i != 0) {
+                $("<li/>")
+                    .attr("data-target", "#feeds")
+                    .attr("data-slide-to", "" + i)
+                    .appendTo($carousel_indicator);
 
-            var first_inner = $("<div/>")
-                .addClass("item")
-                .appendTo($carousel_inner);
+                var first_inner = $("<div/>")
+                    .addClass("item")
+                    .appendTo($carousel_inner);
 
-            $("<h4/>")
-                .html(feedbacks[0]['name'])
-                .appendTo(first_inner);
+                $("<h4/>")
+                    .html(feedbacks[0]['name'])
+                    .appendTo(first_inner);
 
-            $("<p/>")
-                .html(feedbacks[0]['feedback'])
-                .appendTo(first_inner);
+                $("<p/>")
+                    .html(feedbacks[0]['feedback'])
+                    .appendTo(first_inner);
+            }
         });
     }
 </script>
