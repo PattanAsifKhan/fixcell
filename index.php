4dc7d0710c212d992518341b05b7db133695af66
sanjit <sanjitschouhan@gmail.com>
2017-07-05 19:49:01 +0530

    feedback

--- index.php
+++ index.php
@@ -125,9 +125,9 @@
 
 <?php
 
-error_reporting(E_ALL | E_STRICT);
-ini_set("display_errors", 1);
-ini_set("html_errors", 1);
+//error_reporting(E_ALL | E_STRICT);
+//ini_set("display_errors", 1);
+//ini_set("html_errors", 1);
 
 $conn = mysqli_connect('localhost', 'root', 'avi', 'fixcell');
 
@@ -296,5 +296,8 @@ echo "</script>";
         background-color: white;
     }
 
+    #carousel-inner div {
+        margin: 20%;
+    }
 
 </style>
