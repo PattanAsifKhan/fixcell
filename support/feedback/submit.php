8759d5b625a904795bff52fa69cfe7b4cf8aca42
sanjit <sanjitschouhan@gmail.com>
2017-07-05 18:41:44 +0530

    feedback

--- support/feedback/submit.php
+++ support/feedback/submit.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", 1);
+ini_set("html_errors", 1);
 $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
