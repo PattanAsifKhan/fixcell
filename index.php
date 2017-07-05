d05113454c000df6901555175789c57e0ee7465d
sanjit <sanjitschouhan@gmail.com>
2017-07-05 23:03:15 +0530

    feedback

--- index.php
+++ index.php
@@ -157,11 +157,11 @@ echo "</script>";
         </div>
 
         <!-- Left and right controls -->
-        <a class="left carousel-control" href="#feeds" data-slide="prev" style="background-color: transparent">
+        <a class="left carousel-control" href="#feeds" data-slide="prev" style="background: transparent !important">
             <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#feeds" data-slide="next" style="background-color: transparent">
+        <a class="right carousel-control" href="#feeds" data-slide="next" style="background: transparent !important">
             <span class="glyphicon glyphicon-chevron-right"></span>
             <span class="sr-only">Next</span>
         </a>
