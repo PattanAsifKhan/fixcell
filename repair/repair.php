454f023bdf636ad4ffbda5aa54370182cdd314b6
sanjit <sanjitschouhan@gmail.com>
2017-07-09 01:28:06 +0530

    mysql implementation

--- repair/repair.php
+++ repair/repair.php
@@ -160,7 +160,7 @@ if (isset($_GET['brand']) and isset($_GET['model'])) {
         var service_price = $("<h5/>")
             .addClass("text-primary")
             .addClass("badge")
-            .html("₹ " + services[i][1] + "/-")
+            .html("₹ " + parseFloat(services[i][1]).toFixed(2) + "/-")
             .appendTo(panel_body);
 
     });
